1ac1f0686a2f8b7e92c7ff905a04738886d63cc6
Seyoung Hong <maiorem00@gmail.com>
2024-11-01 03:04:37 +0900

    feat : 비관적 락 제거

--- src/main/java/com/hhplus/io/concert/persistence/SeatJpaRepository.java
+++ src/main/java/com/hhplus/io/concert/persistence/SeatJpaRepository.java
@@ -13,6 +13,5 @@ import java.util.Optional;
 public interface SeatJpaRepository extends JpaRepository<Seat, Long> {
     List<Seat> findAllByConcertIdAndStatus(Long concertDateId, String status);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Seat> findBySeatId(Long seatId);
 }
