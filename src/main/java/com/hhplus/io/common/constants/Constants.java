a00eb53ae0ada86815edfbcc3a5af181e475dc5c
Seyoung Hong <maiorem00@gmail.com>
2024-11-08 00:36:33 +0900

    feat : 레디스 키 상수처리

--- src/main/java/com/hhplus/io/common/constants/Constants.java
+++ src/main/java/com/hhplus/io/common/constants/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
     public static class CacheText {
         public static final String SEPARATOR = "!@";
+        public static final String RESERVE_SEAT_KEY_PREFIX = "reserveSeat:";
     }
 
 }
