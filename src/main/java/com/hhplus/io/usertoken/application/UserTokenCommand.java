fe008f1e5716b57754277ff5a6432e1621f12521
Seyoung Hong <maiorem00@gmail.com>
2024-11-08 00:23:34 +0900

    feat : 대기열 발급 로직 수정

--- src/main/java/com/hhplus/io/usertoken/application/UserTokenCommand.java
+++ src/main/java/com/hhplus/io/usertoken/application/UserTokenCommand.java
@@ -1,9 +1,8 @@
 package com.hhplus.io.usertoken.application;
 
-import java.time.LocalDateTime;
 
-public record UserTokenCommand(Long userId, Long sequence, String token, LocalDateTime expiresAt) {
-    public static UserTokenCommand of(Long userId, Long sequence, String token, LocalDateTime tokenExpire) {
-        return new UserTokenCommand(userId, sequence, token, tokenExpire);
+public record UserTokenCommand(Long userId, Long sequence, String token) {
+    public static UserTokenCommand of(Long userId, Long sequence, String token) {
+        return new UserTokenCommand(userId, sequence, token);
     }
 }
