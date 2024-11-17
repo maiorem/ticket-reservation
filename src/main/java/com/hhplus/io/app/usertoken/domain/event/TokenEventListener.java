7ec9060eee083aa3f87f417f41414a328b58bc67
Seyoung Hong <maiorem00@gmail.com>
2024-11-17 14:05:14 +0900

    refact : 이벤트 리스너 도메인별로 분리

--- src/main/java/com/hhplus/io/app/usertoken/domain/event/TokenEventListener.java
+++ src/main/java/com/hhplus/io/app/usertoken/domain/event/TokenEventListener.java
@@ -1,7 +1,11 @@
 package com.hhplus.io.app.usertoken.domain.event;
 
+import com.hhplus.io.app.reservation.domain.event.ReservationEvent;
 import com.hhplus.io.app.usertoken.domain.service.WaitingQueueService;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class TokenEventListener {
@@ -12,7 +16,9 @@ public class TokenEventListener {
         this.waitingQueueService = waitingQueueService;
     }
 
-    public void tokenExpired(String token) {
-        waitingQueueService.expireToken(token);
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void tokenExpired(ReservationEvent event) {
+        waitingQueueService.expireToken(event.token());
     }
 }
