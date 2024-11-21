7ce120566f92e55f578ec9e2da74c1c7616d1c25
Seyoung Hong <maiorem00@gmail.com>
2024-11-21 23:17:58 +0900

    feat : outbox 스케쥴러 - 발행완료 되지 않고 5분 지난 이벤트 처리

--- src/main/java/com/hhplus/io/app/event/application/scheduler/OutboxScheduler.java
+++ src/main/java/com/hhplus/io/app/event/application/scheduler/OutboxScheduler.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -26,23 +27,25 @@ public class OutboxScheduler {
     @Transactional
     public void processOutboxEvents() {
         List<Outbox> pendingEvents = outboxService.findPendingEvents();
+        LocalDateTime now = LocalDateTime.now();
         for (Outbox event : pendingEvents) {
-            try {
-                String topic = "";
-                switch (event.getAggregateType()) {
-                    case "TEMP_RESERVE" -> topic = Constants.KafkaTopics.TEMP_RESERVE_TOPIC;
-                    case "PAYMENT" -> topic = Constants.KafkaTopics.PAYMENT_TOPIC;
+            //5분이 지나도록 발행되지 않은 outbox에 대한 처리
+            if (event.getCreatedAt().isBefore(now.minusMinutes(5))) {
+                try {
+                    String topic = "";
+                    switch (event.getDomainType()) {
+                        case "TEMP_RESERVE" -> topic = Constants.KafkaTopics.TEMP_RESERVE_TOPIC;
+                        case "PAYMENT" -> topic = Constants.KafkaTopics.PAYMENT_TOPIC;
+                    }
+                    event.published();
+                    outboxService.save(event);
+                    kafkaMessageProducer.send(topic, event.getEventKey(), event.getMessage());
+
+                } catch (Exception e) {
+                    log.error("Failed to process outbox event ID: {} , Error: {}", event.getOutboxId(), e.getMessage());
                 }
-
-                kafkaMessageProducer.send(topic, event.getAggregateId(), event.getMessage());
-                event.published();
-                outboxService.save(event);
-
-            } catch (Exception e) {
-                log.error("Failed to process outbox event ID: {} , Error: {}", event.getOutboxId(), e.getMessage());
-                // 실패처리
-
             }
+
         }
     }
 }
